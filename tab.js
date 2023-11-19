4b325939e43e9b6dbc5dd9449316913233018760
hiroko01 <hirokosakamoto31@gmail.com>
2023-11-19 18:42:06 +0900

    クリックしたらイベントが起こる１つ作った

--- tab.js
+++ tab.js
@@ -5,5 +5,18 @@
     const $nav = $tab.querySelectorAll('[data-nav]');
     const $content = $tab.querySelectorAll('[data-content]');
 
-    console.log('$content',$content);
+
+    //initはinitializeの略で初期化という意味
+    //１つ目の要素だけ見えるようにしたいからblockを使う
+    const init = () => {
+        $content[0].style.display = 'block';
+    };
+    init();
+    //クリックしたらイベントが起こる
+    const handleClick = (e) => {
+        e.preventDefault();
+        console.log('Clicled!');
+    };
+    $nav[0].addEventListener('click',(e) => handleClick(e));
+
 })();
