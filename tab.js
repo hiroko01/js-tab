4dd8f3996147535514cdefd8cad740abe3f52d86
hiroko01 <hirokosakamoto31@gmail.com>
2023-11-19 22:04:23 +0900

    リファクタリング完了

--- tab.js
+++ tab.js
@@ -4,6 +4,8 @@
     const $tab = $doc.getElementById('js-tab');
     const $nav = $tab.querySelectorAll('[data-nav]');
     const $content = $tab.querySelectorAll('[data-content]');
+    const ACTIVE_CLASS = 'is-active';
+    const navLen = $nav.length;
 
 
     //initはinitializeの略で初期化という意味
@@ -23,10 +25,10 @@
         //対象外のnav,content全て一旦リセットする コンテンツエリアを全て非表示にする
         //クリックされた部分に応じてコンテンツ部分が変化する
         let index = 0;
-        while (index < $nav.length){
+        while (index < navLen){
             $content[index].style.display = 'none';
             //ここに足す　「remove」=classを排除するという命令
-            $nav[index].classList.remove('is-active')
+            $nav[index].classList.remove(ACTIVE_CLASS)
             index++;
         }
 
@@ -37,14 +39,14 @@
         //クリックされたらnavのデータ属性に応じて対応するコンテンツエリアのDOMを取得して表示させる
         $tab.querySelectorAll('[data-content ="' + targetVal + '"]')[0].style.display = 'block';
         //DOM属性についているclass属性の一覧化を取得,「add」=リストの中に文字列を追加する（is-active が追加、タブがアクティブ（黒）になる）
-        $nav[ targetVal ].classList.add('is-active');
+        $nav[ targetVal ].classList.add(ACTIVE_CLASS);
         console.log('$nav[ targetVal ].classList',$nav[ targetVal ].classList);
         //クリックした時に必要なところだけ見えるようにする　↑23行目へ
     };
 
     //全nav要素に対して関数を適用・発火
     let index = 0;
-    while (index < $nav.length){
+    while (index < navLen){
         $nav[index].addEventListener('click',(e) => handleClick(e));
         index++;
     }
